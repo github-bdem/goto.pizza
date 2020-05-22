179bfb91251fa63c082677efdee664b81fd0bd4a
bdell <brandon@bdell.dev>
2020-05-21 23:16:41 -0700

    Adds leaflet map and scaffolds out sidebar
    Adds leaflet map and scaffolds out sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import PizzaMap from './components/PizzaMap';
+import { PizzaMap, SearchSidebar } from './components';
 
 import './App.scss';
 
 const App = () => (
     <div className="App">
         <div className="MainMapContainer">
+            <SearchSidebar />
             <PizzaMap />
         </div>
     </div>
