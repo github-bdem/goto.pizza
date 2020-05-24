8147b6240a7c3a15f7d971cff2444ae70cf5cd9a
bdell <brandon@bdell.dev>
2020-05-23 17:18:32 -0700

    Scaffolds out basic App.js shape
    Scaffolds out basic App.js shape

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { PizzaMap, SearchSidebar } from './components';
 
 import './App.scss';
 
-const App = () => (
-    <div className="App">
-        <div className="MainMapContainer">
-            <SearchSidebar />
-            <PizzaMap />
+const App = () => {
+    const [mapCenter, setMapCenter] = useState({ lat: -0.09, lng: 51.505 });
+    const [pizzaLocations, setPizzaLocations] = useState([]);
+    const [selectedLocation, setSelectedLocation] = useState(null);
+
+    useEffect(() => {
+        // CLEAR SELECTED LOCATION
+        // CALL GMAPS API
+        // SET PIZZA LOCATIONS
+    }, [mapCenter]);
+
+    return (
+        <div className="App">
+            <div className="MainMapContainer">
+                <SearchSidebar pizzaLocations={pizzaLocations} selectedLocation={selectedLocation} />
+                <PizzaMap pizzaLocations={pizzaLocations} setSelectedLocation={setSelectedLocation} />
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default App;
