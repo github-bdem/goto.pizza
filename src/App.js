b1003f9e9691e29b14e7c22b238c823649d0e8c9
bdell <brandon.demello@gmail.com>
2020-05-25 03:17:59 -0700

    Hooks up sidebar querying

--- src/App.js
+++ src/App.js
@@ -6,15 +6,22 @@ import './App.scss';
 const App = () => {
     const [pizzaLocations, setPizzaLocations] = useState([]);
     const [selectedLocation, setSelectedLocation] = useState(null);
+    const [googleMapsSearchService, setGoogleMapsSearchService] = useState(null);
 
     return (
         <div className="App">
             <div className="MainMapContainer">
-                <SearchSidebar pizzaLocations={pizzaLocations} selectedLocation={selectedLocation} />
+                <SearchSidebar
+                    pizzaLocations={pizzaLocations}
+                    selectedLocation={selectedLocation}
+                    googleMapsSearchService={googleMapsSearchService}
+                />
                 <PizzaMap
                     pizzaLocations={pizzaLocations}
                     setSelectedLocation={setSelectedLocation}
                     setPizzaLocations={setPizzaLocations}
+                    googleMapsSearchService={googleMapsSearchService}
+                    setGoogleMapsSearchService={setGoogleMapsSearchService}
                 />
             </div>
         </div>
