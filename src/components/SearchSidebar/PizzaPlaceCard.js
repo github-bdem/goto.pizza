7445fddd2a4138ba08b4b6df853ad2420da59358
WordsPerMinute <github@coreyhodge.net>
2020-07-11 20:33:16 -0600

    quick test on github

--- src/components/SearchSidebar/PizzaPlaceCard.js
+++ src/components/SearchSidebar/PizzaPlaceCard.js
@@ -14,6 +14,7 @@ const PizzaPlaceCard = (props) => {
         markerHoveredLocation,
         setSidebarHovered,
     } = props;
+
     const [isExpanded, setIsExpanded] = useState(false);
     const [isLoadingMoreDetails, setIsLoadingMoreDetails] = useState(false);
     const [detailedInformation, setDetailedInformation] = useState({});
